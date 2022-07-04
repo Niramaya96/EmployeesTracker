25415da6f806b77e7c19b43e329ffacdbe828cc3
Никита <@DESKTOP-UNNMCAA>
2022-07-04 15:03:36 +0300

    Fixed

--- Employee/Employee.cs
+++ Employee/Employee.cs
@@ -6,14 +6,14 @@ namespace Employee
     internal class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } 
         
         public string Surname { get; set; }
 
         public string DateOfBirth { get; set; }
         public string Adress { get; set; }
         public string Department { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
 
 
