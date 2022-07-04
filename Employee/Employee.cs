c5a890a8a54599444b2c0baf2d1c544974c9452b
Никита <@DESKTOP-UNNMCAA>
2022-07-04 14:46:07 +0300

    Добавьте файлы проекта.

---
+++ Employee/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Employee
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Surname { get; set; }
+
+        public string DateOfBirth { get; set; }
+        public string Adress { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+
+
+
+
+    }
+}
