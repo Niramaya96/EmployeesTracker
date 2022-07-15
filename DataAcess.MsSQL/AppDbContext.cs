9cdcf4d4aa419eca2eff254d1931340596902fe5
Никита <@DESKTOP-UNNMCAA>
2022-07-15 15:04:32 +0700

    Fixed

---
+++ DataAcess.MsSQL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Interfaces.DataAcess;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAcess.MsSQL
+{
+    public class AppDbContext : DbContext, IDbContext, IReadDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees => throw new NotImplementedException();
+        public DbSet<Department> Departments => throw new NotImplementedException();
+
+        IQueryable<Employee> IReadDbContext.Employees => throw new NotImplementedException();
+        IQueryable<Department> IReadDbContext.Departments => throw new NotImplementedException();
+    }
+}
